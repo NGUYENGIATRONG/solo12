2e2872460e75c4e00cea4f62a536f6ef671f7ae8
nnq962 <quyet.nguyen.official@gmail.com>
2024-08-23 14:18:28 +0700

    updateaug23p2

--- testrobot.py
+++ testrobot.py
@@ -1,7 +1,7 @@
 from simulation import solo_pybulet_env
 import numpy as np
 action_temp = [0] * 20
-robot = solo12_pybulet_env.Solo12PybulletEnv(on_rack=False)
+robot = solo_pybulet_env.Solo12PybulletEnv(on_rack=False)
 steps = 20000000
 action = [0] * 20
 amplitude = 0.5
